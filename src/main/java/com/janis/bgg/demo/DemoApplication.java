563e697b2fd1c7200fcee502302d05cbd474d1df
Janis <janis666@gmail.com>
2019-08-28 14:57:40 +0200

    init

--- src/main/java/com/janis/bgg/demo/DemoApplication.java
+++ src/main/java/com/janis/bgg/demo/DemoApplication.java
@@ -3,6 +3,13 @@ package com.janis.bgg.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.janis.bgg.demo.Entity")
@@ -12,4 +19,27 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    public static void Browse(String url) {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @EventListener({ApplicationReadyEvent.class})
+    void applicationReadyEvent() {
+        System.out.println("Application started ... launching browser now");
+        Browse("localhost:8080");
+    }
 }
