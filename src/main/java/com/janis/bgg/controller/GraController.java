242adff691a213a5fd85aba98ce8f4202c02ff2b
Janis <janis666@gmail.com>
2019-10-24 01:05:40 +0200

    dodałem swaggera i przerzucam rzeczy do controllera. RestController będzie tylko od restów

--- src/main/java/com/janis/bgg/controller/GraController.java
+++ src/main/java/com/janis/bgg/controller/GraController.java
@@ -1,12 +1,18 @@
 package com.janis.bgg.controller;
 
 import com.janis.bgg.dao.SettingsDao;
+import com.janis.bgg.entities.dto.GraDto;
+import com.janis.bgg.entities.dto.SearchCriteria;
 import com.janis.bgg.entities.entity.Settings;
 import com.janis.bgg.service.GryService;
+import com.janis.bgg.service.ImportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 import static com.janis.bgg.constants.AppConstants.USERNAME;
 
@@ -15,6 +21,9 @@ public class GraController {
 
     @Autowired
     private GryService gryService;
+
+    @Autowired
+    private ImportService importService;
 
     @Autowired
     private SettingsDao settingsDao;
@@ -34,6 +43,42 @@ public class GraController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("gry", gryService.findAll());
         modelAndView.setViewName("gry");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/import")
+    public ModelAndView importMyGames(@RequestParam(value = "userName", required = false) String userName) {
+        ModelAndView model = new ModelAndView();
+        model.addObject("gry", importService.importGamesFromBgg(userName));
+        model.setViewName("gry");
+        return model;
+    }
+
+    @GetMapping(value = "/search")
+    public ModelAndView searchForm() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("searchCriteria", new SearchCriteria());
+        model.setViewName("search");
+        return model;
+    }
+
+    @PostMapping(value = "/search")
+    public ModelAndView searchForm(@ModelAttribute SearchCriteria searchCriteria) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("gry");
+        model.addObject("searchCriteria", searchCriteria);
+        List<GraDto> foundGames = gryService.findAllGamesToDto(searchCriteria);
+        model.addObject("gry", foundGames);
+        return model;
+    }
+
+    @ExceptionHandler
+    public ModelAndView handleErrors(HttpServletRequest request, Exception ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", ex);
+        modelAndView.addObject("url", request.getRequestURL());
+
+        modelAndView.setViewName("error");
         return modelAndView;
     }
 }
---
+++ src/main/java/com/janis/bgg/swagger/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.janis.bgg.swagger;
+
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.janis.bgg.controller"))
+                .paths(getSwaggerPaths())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500)
+                                .message("500 message")
+                                .responseModel(new ModelRef("string"))
+                                .build(),
+                        new ResponseMessageBuilder().code(403)
+                                .message("Forbidden!!!!!")
+                                .build()));
+    }
+
+    private Predicate<String> getSwaggerPaths() {
+        return or(regex("/api.*"),
+                regex("/test.*"));
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(null, null, null, null, new Contact(null, null, null), null, null, newArrayList());
+        return apiInfo;
+    }
+}
