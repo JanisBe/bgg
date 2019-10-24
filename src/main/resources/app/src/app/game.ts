87fdddf16df0e37cd7c4751507f026617428e097
Janis <janis666@gmail.com>
2019-10-24 18:30:38 +0200

    dodanie Angulara

---
+++ src/main/resources/app/src/app/game.ts
@@ -0,0 +1,17 @@
+export class Game {
+  gameId:        number;
+  name:          string;
+  image:         string;
+  thumbnail:     string;
+  minPlayers:    number;
+  maxPlayers:    number;
+  playingTime:   number;
+  yearPublished: number;
+  bggRating:     number;
+  averageRating: number;
+  rank:          number;
+  numPlays:      number;
+  rating:        number;
+  userComment:   string;
+  expansionId:   number
+}
